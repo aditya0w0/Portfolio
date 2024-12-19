e6574949852245a39dc658d9c7b9371ae7d259ae
aditya0w0 <adityagalangtricahaya@gmail.com>
2024-12-19 15:43:12 +0700

    my first portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        elysia:{
+          light: '#FFD6E8',
+          DEFAULT: '#F2A1C1', // Pink pastel
+          accent: '#B6D6F2', // Biru pastel
+        },
+        yaeMiko: {
+          light: '#E572A5', // Magenta khas
+          DEFAULT: '#B084CC', // Ungu pastel
+          accent: '#FFEEC0', // Emas lembut
+        },
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
