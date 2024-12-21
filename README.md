015b8764c649133b72e387819114b9568e4075aa
adityagalangtricahaya <adityagalangtricahaya@gmail.com>
2024-12-21 10:29:13 +0000

    New Style Also Fixed Horizontal Scroll

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Hey there! This is my very first attempt at building a personal portfolio websit
 ---
 
 ### 🛠️ Built With:
+
 - ![HTML5](https://img.shields.io/badge/-HTML5-orange?style=flat-square&logo=html5&logoColor=white)
 - ![CSS3](https://img.shields.io/badge/-CSS3-blue?style=flat-square&logo=css3&logoColor=white)
 - ![Tailwind CSS](https://img.shields.io/badge/-Tailwind_CSS-teal?style=flat-square&logo=tailwind-css&logoColor=white)
@@ -12,14 +13,17 @@ Hey there! This is my very first attempt at building a personal portfolio websit
 ---
 
 ### ⏳ Time Spent:
+
 ![WakaTime](https://wakatime.com/badge/user/76364797-6303-4342-9567-48d03e3a7fa3.svg)
 
 ---
 
 ### ✨ What's Next?
+
 For now, this site is super basic. I'm still learning and figuring things out. Stay tuned for updates and improvements!
 
 ---
 
 ### 📸 Sneak Peek:
+
 > Coming Soon!
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content:[
-    "./*.html", "./src/**/*.{html,js}"
-  ],
+  content: ["./*.html", "./src/**/*.{html,js}"],
   theme: {
     container: {
       center: true,
       padding: "16px",
     },
     extend: {
-      fontFamily:{
+      fontFamily: {
         inter: ["Inter", "sans-serif"],
         poppins: ["Poppins", "sans-serif"],
-        clash: ['Clash Display', 'sans-serif'],
+        clash: ["Clash Display", "sans-serif"],
       },
       colors: {
-        elysia:{
-          light: '#FFD6E8',
-          DEFAULT: '#F2A1C1', // Pink pastel
-          accent: '#B6D6F2', // Biru pastel
+        elysia: {
+          light: "#FFD6E8",
+          DEFAULT: "#F2A1C1", // Pink pastel
+          accent: "#B6D6F2", // Biru pastel
         },
         yaeMiko: {
-          light: '#E572A5', // Magenta khas
-          DEFAULT: '#B084CC', // Ungu pastel
-          accent: '#FFEEC0', // Emas lembut
+          light: "#E572A5", // Magenta khas
+          DEFAULT: "#B084CC", // Ungu pastel
+          accent: "#FFEEC0", // Emas lembut
         },
         dark: "#0f172a",
       },
@@ -32,13 +30,13 @@ module.exports = {
       },
       keyframes: {
         gradientAnimation: {
-          '0%': { backgroundPosition: '0% 50%' },
-          '50%': { backgroundPosition: '100% 50%' },
-          '100%': { backgroundPosition: '0% 50%' },
+          "0%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+          "100%": { backgroundPosition: "0% 50%" },
         },
       },
       animation: {
-        gradient: 'gradientAnimation 10s ease infinite',
+        gradient: "gradientAnimation 10s ease infinite",
       },
     },
   },
